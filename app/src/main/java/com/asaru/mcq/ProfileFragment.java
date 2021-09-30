1e4d305e038bfbc734f4ad4fee6f3f2334a160e9
Asaru Eshada <asaru4321eshada@gmail.com>
2021-09-30 13:42:02 +0530

    change language

--- app/src/main/java/com/asaru/mcq/ProfileFragment.java
+++ app/src/main/java/com/asaru/mcq/ProfileFragment.java
@@ -1,64 +1,71 @@
 package com.asaru.mcq;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button changeLanguage;
+    private String radiobutton;
+    Context context;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        changeLanguage = view.findViewById(R.id.changeLang);
+
+        changeLanguage.setOnClickListener(v -> openChangeLanguage());
+        return view;
     }
+
+    private void openChangeLanguage() {
+        String[] language = new String[]{"සිංහල", "தமிழ்", "English"};
+        radiobutton = language[0];
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Choose the Language");
+        builder.setSingleChoiceItems(language, 0, (dialog, which) -> radiobutton = language[which]);
+        builder.setPositiveButton("Save", (dialog, which) -> changeLanguageSave());
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
+    public void changeLanguageSave() {
+        String lang;
+        if (radiobutton.equals("English")) {
+            lang = "en";
+            context = ChangeLanguage.setLocale(getActivity(), "en");
+
+
+        } else {
+            if (radiobutton.equals("සිංහල")) {
+                lang = "si";
+                context = ChangeLanguage.setLocale(getActivity(), "si");
+
+            } else {
+                lang = "ta";
+                context = ChangeLanguage.setLocale(getActivity(), "ta");
+
+            }
+        }
+        SharedPreferences settings = getContext().getSharedPreferences("language", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.remove("Language");
+        editor.putString("Language", lang);
+        Log.d("Language", lang);
+        editor.apply();
+    }
+
 }
